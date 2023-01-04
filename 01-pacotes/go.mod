49ce03d935421cc2ab2739249cdd9f7970233ccf
Danilo A. Silva <danilo.official22@hotmail.com>
2023-01-04 12:53:46 -0300

    02-pacotesExternos

--- 01-pacotes/go.mod
+++ 01-pacotes/go.mod
@@ -1,3 +1,5 @@
 module modulo-test
 
 go 1.19
+
+require github.com/badoux/checkmail v1.2.1 // indirect
--- 01-pacotes/main.go
+++ 01-pacotes/main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"modulo-test/auxiliar"
 	"fmt"
+	"github.com/badoux/checkmail"
 )
 
 func main () {
 	auxiliar.Escrever()
 	fmt.Println("vindo do main")
+
+	erro0 := checkmail.ValidateFormat("devmail@mail.com")
+	fmt.Println(erro0)
+
+	erro1 := checkmail.ValidateFormat("1234")
+	fmt.Println(erro1)
 }
